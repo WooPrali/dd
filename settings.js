c81bf2818bc32d3dd5cbdee58b792d1628d99de8
nagkhumbu <nagasai.mudara@khumbusystems.com>
2021-07-16 19:46:34 +0530

    Update

--- settings.js
+++ settings.js
@@ -1,4 +1,25 @@
 
+(function($){
+  $(document).ready(function(){
+    var $body=$("body");
+    function advanced_settings(){
+      var $el=$(".advanced_settings");
+      var $btn=$(".advanced_settings_btn");
+      $btn.click(function(){
+        if($el.hasClass("active")){
+          $el.removeClass("active");
+          $btn.removeClass("active");
+        }else{
+          $el.addClass("active");
+          $btn.addClass("active");
+        }       
+      });  
+    }
+    advanced_settings();
+  })
+})(jQuery);
+
+
 function onUpdate(key, value) {
   //Wix.Settings.triggerSettingsUpdatedEvent({key: key, value: value});
   console.log(key);
