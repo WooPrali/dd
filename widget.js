ae0a94f802ab4deb6f458d04fba683fe0d4fe42c
nagkhumbu <nagasai.mudara@khumbusystems.com>
2021-07-20 16:53:50 +0530

    update

--- widget.js
+++ widget.js
@@ -1,8 +1,8 @@
 Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, onSettingsUpdate);
 // You can get the style params programmatically, un-comment the following snippet to see how it works:
-/*Wix.Styles.getStyleParams(style => {
+Wix.Styles.getStyleParams(style => {
  console.log(style);
- });*/
+ });
 
 // You can also get the style every time it changes, try this:
 /*Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
